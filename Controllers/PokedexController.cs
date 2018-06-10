620a21b7b56c4fc2d18d1b74c1aa3a85a80c8b59
NguyenHaPhuong <NguyenHaPhuong@users.noreply.github.com>
2018-06-10 19:18:44 +0800

    PokemonHouse

---
+++ Controllers/PokedexController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PokemonHouse.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PokemonHouse.Controllers
+{
+    public class PokedexController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult Number(int id)
+		{
+			Pokedex poke = PokedexMgr.GetPokemon(id);
+
+			if (poke == null)
+			{
+				ViewData["Num"] = id.ToString();
+				return View("NotFound");
+
+			}
+
+			else
+
+			{
+				ViewData["Pokemon"] = poke;
+				return View();
+			}
+		}
+	}
+}
---
+++ Models/Pokedex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonHouse.Models
+{
+   public class Pokedex
+   {
+      public int ID { get; private set; }
+      public string Name { get; private set; }
+      public string Type1 { get; private set; }
+      public string Type2 { get; private set; }
+      public int Attack { get; private set; }
+      public int Defence { get; private set; }
+      public int Stamina { get; private set; }
+
+      public Pokedex(int id, string name, 
+                      string type1, string type2, 
+                      int attack, int defence, int stamina)
+      {
+         ID = id;
+         Name = name;
+         Type1 = type1;
+         Type2 = type2;
+         Attack = attack;
+         Defence = defence;
+         Stamina = stamina;
+      }
+
+   }
+
+}
